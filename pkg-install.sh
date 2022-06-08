d137ce9ae4e8d0d4577c99f6ecb8a2c19127ff65
techtycho <omarahmedabdelhakim@gmail.com>
2022-06-08 16:31:44 +0200

    Add the dependencies

--- pkg-install.sh
+++ pkg-install.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+sudo pacman -S awesome dmenu alacritty nitrogen gedit neovim vim lxappearance neofetch
+yay -S picom-jonaburg-git nerd-fonts-hack brave-bin kripton-theme-git
+
