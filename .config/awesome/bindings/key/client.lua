f270c5d07260d33730e555da722f8ad8d012d38f
techtycho <omarahmedabdelhakim@gmail.com>
2023-01-22 21:40:44 +0200

    Add more bindings

--- .config/awesome/bindings/key/client.lua
+++ .config/awesome/bindings/key/client.lua
@@ -5,55 +5,78 @@ local modkey = bind.modkey
 
 client.connect_signal("request::default_keybindings", function()
   bind.keygroup(bind.mode.client_, "client", {
-    awful.key({ modkey, }, "f",
+    -- Toggle Fullscreen | Super + f
+    awful.key({ modkey }, "f",
       function(c)
         c.fullscreen = not c.fullscreen
         c:raise()
       end,
       { description = "Toggle fullscreen" }),
 
-    awful.key({ modkey, "Shift" }, "c", function(c) c:kill() end,
-      { description = "Close" }),
+    -- Close Window | Super + Shift + c
+    awful.key({ modkey, "Shift" }, "c",
+      function(c)
+        c:kill()
+      end,
+      { description = "Close window" }),
 
-    awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle,
+    -- Toggle Floating | Super + Ctrl + Space
+    awful.key({ modkey, "Control" }, "space",
+      awful.client.floating.toggle,
       { description = "Toggle floating" }),
 
-    awful.key({ modkey, "Control" }, "Return", function(c) c:swap(awful.client.getmaster()) end,
+    -- Move To Master | Super + Ctrl + Return
+    awful.key({ modkey, "Control" }, "Return",
+      function(c)
+        c:swap(awful.client.getmaster())
+      end,
       { description = "Move to master" }),
 
-    awful.key({ modkey, }, "o", function(c) c:move_to_screen() end,
+    -- Move To Screen | Super + o
+    awful.key({ modkey }, "o",
+      function(c)
+        c:move_to_screen()
+      end,
       { description = "Move to screen" }),
 
-    awful.key({ modkey, }, "t", function(c) c.ontop = not c.ontop end,
+    -- Toggle Keep On Top | Super + t
+    awful.key({ modkey }, "t",
+      function(c)
+        c.ontop = not c.ontop
+      end,
       { description = "Toggle keep on top" }),
 
-    awful.key({ modkey, }, "n",
+    -- Minimize Client | Super + n
+    awful.key({ modkey }, "n",
       function(c)
         -- The client currently has the input focus, so it cannot be
         -- minimized, since minimized clients can't have the focus.
         c.minimized = true
       end,
-      { description = "Minimize" }),
+      { description = "Minimize client" }),
 
-    awful.key({ modkey, }, "m",
+    -- (Un)Maximize Window | Super + m
+    awful.key({ modkey }, "m",
       function(c)
         c.maximized = not c.maximized
         c:raise()
       end,
-      { description = "(Un)Maximize" }),
+      { description = "(Un)Maximize window" }),
 
+    -- (Un)Maximize Window Vertically | Super + Ctrl + m
     awful.key({ modkey, "Control" }, "m",
       function(c)
         c.maximized_vertical = not c.maximized_vertical
         c:raise()
       end,
-      { description = "(Un)Maximize vertically" }),
+      { description = "(Un)Maximize window vertically" }),
 
+    -- (Un)Maximize Window Horrizontally | Super + Shift + m
     awful.key({ modkey, "Shift" }, "m",
       function(c)
         c.maximized_horizontal = not c.maximized_horizontal
         c:raise()
       end,
-      { description = "(Un)Maximize horizontally" }),
+      { description = "(Un)Maximize window horizontally" }),
   })
 end)
